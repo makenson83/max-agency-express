169f3806e1272278f48d4bb3fad97cd1392680c2
Makenson Simolien <118572809+makenson83@users.noreply.github.com>
2025-06-08 12:41:56 -0300

    Update taxa.js

--- taxa.js
+++ taxa.js
@@ -1,13 +1,11 @@
 
-const fetch = require('node-fetch');
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
-    const response = await fetch('https://api.exchangerate.host/latest?base=BRL&symbols=HTG');
+    const response = await fetch("https://api.exchangerate.host/latest?base=BRL&symbols=HTG");
     const data = await response.json();
     const taxa = data.rates.HTG;
     res.status(200).json({ taxa });
-  } catch (erro) {
+  } catch (err) {
     res.status(500).json({ erro: "Erro ao buscar taxa online." });
   }
-};
+}
