7f4de0a365303d315eadba6559423d39768aee29
Makenson Simolien <118572809+makenson83@users.noreply.github.com>
2025-06-09 18:56:28 -0300

    Add files via upload

--- taxa.js
+++ taxa.js
@@ -1,10 +1,1 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://api.exchangerate.host/latest?base=BRL&symbols=HTG");
-    const data = await response.json();
-    const taxa = data.rates.HTG;
-    res.status(200).json({ taxa });
-  } catch (err) {
-    res.status(500).json({ erro: "Erro ao buscar taxa online." });
-  }
-}
+// Código do backend para conversão
