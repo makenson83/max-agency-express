ec68cb4e82cda678d0fc4eb9cbe24150dc071007
Makenson Simolien <118572809+makenson83@users.noreply.github.com>
2025-07-26 17:49:14 -0300

    Update github-webhook.js

--- api/github-webhook.js
+++ api/github-webhook.js
@@ -1,1 +1,10 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('📦 Webhook recebido do GitHub!');
+    console.log(req.body); // Para testes, pode visualizar os dados enviados
 
+    return res.status(200).json({ status: 'Recebido com sucesso' });
+  }
+
+  return res.status(405).json({ error: 'Método não permitido' });
+}
