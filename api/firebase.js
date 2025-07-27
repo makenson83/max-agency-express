55085b9ce1edbe0dfd903b8c734b1c60b3fae7d0
Makenson Simolien <118572809+makenson83@users.noreply.github.com>
2025-07-27 13:56:31 -0300

    Update firebase.js

--- api/firebase.js
+++ api/firebase.js
@@ -1,1 +1,18 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc, updateDoc } from "firebase/firestore";
 
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBVarvB2l1nKwtfTwS3nCbV3HIdC7",
+  authDomain: "max-agency-express.firebaseapp.com",
+  projectId: "max-agency-express",
+  storageBucket: "max-agency-express.appspot.com",
+  messagingSenderId: "4907592552",
+  appId: "1:4907592552:web:5f563f7939b10860b0",
+  measurementId: "G-PBX13GCT9"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
